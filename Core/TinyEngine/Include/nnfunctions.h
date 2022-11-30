47534178e46eea12955f8ed89288a8718bf5a5f0
Anne Ouyang <anne.ouyang@gmail.com>
2022-11-30 17:35:09 -0500

    save experiments

--- Core/TinyEngine/Include/nnfunctions.h
+++ Core/TinyEngine/Include/nnfunctions.h
@@ -1081,6 +1081,62 @@ static inline void transpose_depthwise_mac_kernel3_2row_uniweight(q31_t* sum_0, 
     *sum_1 += two_column_buffer[3] * ksrc_transposed[8];
 }
 
+static inline void transpose_depthwise_mac_kernel3_2row_uniweight3(q31_t* sum,  
+                      const q7_t* two_column_buffer, const q7_t* ksrc_transposed, const uint16_t input_width, 
+                      const uint16_t STRIDE, const uint16_t IN_PAD, const uint16_t OUT_PAD) {
+    sum[0] += two_column_buffer[0] * ksrc_transposed[0];
+    sum[1] += two_column_buffer[1] * ksrc_transposed[0];
+    sum[0] += two_column_buffer[1] * ksrc_transposed[1];
+    sum[1] += two_column_buffer[2] * ksrc_transposed[1];
+    sum[0] += two_column_buffer[2] * ksrc_transposed[2];
+    sum[1] += two_column_buffer[3] * ksrc_transposed[2];
+    two_column_buffer += (input_width - 1) * STRIDE + 1 + IN_PAD * 2 + OUT_PAD;
+
+    sum[0] += two_column_buffer[0] * ksrc_transposed[3];
+    sum[1] += two_column_buffer[1] * ksrc_transposed[3];
+    sum[0] += two_column_buffer[1] * ksrc_transposed[4];
+    sum[1] += two_column_buffer[2] * ksrc_transposed[4];
+    sum[0] += two_column_buffer[2] * ksrc_transposed[5];
+    sum[1] += two_column_buffer[3] * ksrc_transposed[5];
+    two_column_buffer += (input_width - 1) * STRIDE + 1 + IN_PAD * 2 + OUT_PAD;
+
+    sum[0] += two_column_buffer[0] * ksrc_transposed[6];
+    sum[1] += two_column_buffer[1] * ksrc_transposed[6];
+    sum[0] += two_column_buffer[1] * ksrc_transposed[7];
+    sum[1] += two_column_buffer[2] * ksrc_transposed[7];
+    sum[0] += two_column_buffer[2] * ksrc_transposed[8];
+    sum[1] += two_column_buffer[3] * ksrc_transposed[8];
+}
+
+static inline void transpose_depthwise_mac_kernel3_2row_uniweight2(q31_t* sum_0, q31_t* sum_1, 
+                      const q7_t* two_column_buffer, const q7_t* ksrc_transposed, const uint16_t input_width, 
+                      const uint16_t STRIDE, const uint16_t IN_PAD, const uint16_t OUT_PAD) {
+    // STRIDE = 1; IN_PAD = 1; OUT_PAD = 0;
+    *sum_0 += two_column_buffer[0] * ksrc_transposed[0];
+    *sum_1 += two_column_buffer[1] * ksrc_transposed[0];
+    *sum_0 += two_column_buffer[1] * ksrc_transposed[1];
+    *sum_1 += two_column_buffer[2] * ksrc_transposed[1];
+    *sum_0 += two_column_buffer[2] * ksrc_transposed[2];
+    *sum_1 += two_column_buffer[3] * ksrc_transposed[2];
+    // two_column_buffer += (input_width - 1) * STRIDE + 1 + IN_PAD * 2 + OUT_PAD;
+    two_column_buffer += (input_width - 1) + 3;
+
+    *sum_0 += two_column_buffer[0] * ksrc_transposed[3];
+    *sum_1 += two_column_buffer[1] * ksrc_transposed[3];
+    *sum_0 += two_column_buffer[1] * ksrc_transposed[4];
+    *sum_1 += two_column_buffer[2] * ksrc_transposed[4];
+    *sum_0 += two_column_buffer[2] * ksrc_transposed[5];
+    *sum_1 += two_column_buffer[3] * ksrc_transposed[5];
+    two_column_buffer += (input_width - 1) + 3;
+
+    *sum_0 += two_column_buffer[0] * ksrc_transposed[6];
+    *sum_1 += two_column_buffer[1] * ksrc_transposed[6];
+    *sum_0 += two_column_buffer[1] * ksrc_transposed[7];
+    *sum_1 += two_column_buffer[2] * ksrc_transposed[7];
+    *sum_0 += two_column_buffer[2] * ksrc_transposed[8];
+    *sum_1 += two_column_buffer[3] * ksrc_transposed[8];
+}
+
 static inline void transpose_depthwise_mac_kernel3_1row_uniweight(q31_t* sum_0, 
                       const q7_t* two_column_buffer, const q7_t* ksrc_transposed, const uint16_t input_width, 
                       const uint16_t STRIDE, const uint16_t IN_PAD, const uint16_t OUT_PAD) {
